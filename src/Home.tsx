2b783833ea420f12e0b39e8d0f9daf477b60a137
philluple <pnl2111@columbia.edu>
2024-06-01 12:12:08 -0700

    fixing

--- src/Home.tsx
+++ src/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
             <div className="mid-section">
                 <div className="large-name">Phillip Le</div>
                 <div className="description"> MY AIM IS TO CREATE BEAUTIFUL, EFFECTIVE, AND RESPONSIBLE SOFTWARE SOLUTIONS. BASED IN NEW YORK AND LOS ANGELES.</div>
-                <div className="contact">LE.PHILLIP@COLUMBIA.EDU</div>
+                <div className="contact">LE.PHILLIP@COLUMBIA.ED</div>
             </div>
 
         </Layout>
